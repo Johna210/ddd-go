b7f3baf9fd85143adeacec4d8fc0faad313e3930
Johna210 <solomonjohna21@gmail.com>
2024-10-22 16:26:16 +0300

    Added in-memory impl test

--- domain/customer/memory/memory_test.go
+++ domain/customer/memory/memory_test.go
@@ -1,5 +1,55 @@
 package memory
 
-import "testing"
+import (
+	"errors"
+	"testing"
 
-func TestMemory_GetCustom(t *testing.T) {}
+	"github.com/google/uuid"
+	"github.com/johna210/ddd-go/aggregate"
+	"github.com/johna210/ddd-go/domain/customer"
+)
+
+func TestMemory_GetCustom(t *testing.T) {
+	type testCase struct {
+		name        string
+		id          uuid.UUID
+		expectedErr error
+	}
+
+	cust, err := aggregate.NewCustomer("Johna")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id := cust.GetID()
+
+	repo := MemoryRepository{
+		customers: map[uuid.UUID]aggregate.Customer{
+			id: cust,
+		},
+	}
+
+	testCases := []testCase{
+		{
+			name:        "no customer by id",
+			id:          uuid.MustParse("88592f09-b78e-408a-a7f1-c68b3ce1f3ad"),
+			expectedErr: customer.ErrCustomerNotFound,
+		},
+
+		{
+			name:        "customer by id",
+			id:          id,
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := repo.Get(tc.id)
+
+			if !errors.Is(err, tc.expectedErr) {
+				t.Errorf("expected error %v, got %v", tc.expectedErr, err)
+			}
+		})
+	}
+}
